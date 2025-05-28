905b7b1c1e239f63d6e0137e16cce4b8fded04ae
KetanAggarwal26 <155048369+KetanAggarwal26@users.noreply.github.com>
2025-05-28 19:07:11 +0000

    asasdc

--- 01basics/datatypeConversion.js
+++ 01basics/datatypeConversion.js
@@ -45,4 +45,30 @@ let negvalue=-value
 // console.log(1 + 2 + '2');
 
 
+// **********Memory*****************
+
+//Stack(Primitive) means copy , heap(Non-primitive) reference of original value if we change one value it will change original as well
+
+let origValue='tets';
+let copyvalue=origValue;
+
+origValue='namememme';
+
+// console.log(origValue);
+// console.log(copyvalue);
+
+let userone={
+    email:'tetsts@gmail.com',
+    upi:'wi@oksbi.com'
+}
+
+let userTwo=userone;
+console.log(userone.email);
+console.log(userTwo.email);
+
+userTwo.email='akak2@kka.com'
+
+ console.log(userone.email);
+console.log(userTwo.email);
+
 
