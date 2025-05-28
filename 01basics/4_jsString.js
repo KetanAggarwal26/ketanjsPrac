f61f2544c4193092a883eb1056db0d388a51d7a0
KetanAggarwal26 <155048369+KetanAggarwal26@users.noreply.github.com>
2025-05-28 19:27:32 +0000

    wedfds

--- 01basics/4_jsString.js
+++ 01basics/4_jsString.js
@@ -1,9 +1,18 @@
-let stingOne='Name'
+const stingOne='Name'
 
 //string interpolation
 console.log(`Hello my name is ${stingOne}`);
 
 
-let stingTwo= new String('addssdfdsdf')
+const stingTwo= new String('addssdfdsdf')
 console.log(stingTwo[0]);
 console.log(stingTwo.__proto__);
+
+console.log(stingTwo.charAt(1));
+console.log(stingTwo.indexOf('d'));
+
+const substingTwo= stingTwo.substring(0,4)
+console.log(substingTwo);
+
+const slicestingTwo= stingTwo.slice(-10,2)
+console.log(slicestingTwo);
