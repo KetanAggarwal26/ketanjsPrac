5c977c64e90bb0f06d016f0f18e16b65a310ed78
KetanAggarwal26 <155048369+KetanAggarwal26@users.noreply.github.com>
2025-05-28 19:17:29 +0000

    string nnew

---
+++ 01basics/4_jsString.js
@@ -0,0 +1,9 @@
+let stingOne='Name'
+
+//string interpolation
+console.log(`Hello my name is ${stingOne}`);
+
+
+let stingTwo= new String('addssdfdsdf')
+console.log(stingTwo[0]);
+console.log(stingTwo.__proto__);
