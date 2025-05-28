bd149e49a376195c2c9fda3f881918979eece039
KetanAggarwal26 <155048369+KetanAggarwal26@users.noreply.github.com>
2025-05-28 17:34:59 +0000

    changegs datatypes

--- 01basics/datatypes.js
+++ 01basics/datatypes.js
@@ -2,6 +2,18 @@
 
 //alert(3 +3); // we are using node js not browser
 
-console.log() 
-console.log()
+let name='test'
+let age=3
+let isLoggedIn=false
 
+//number => 2 to power 53
+//bigint
+//string
+//boolean
+//null => standAlone value
+//undefined => not defined value
+//symbol => unique
+
+//object
+
+console.log(typeof isLoggedIn);
