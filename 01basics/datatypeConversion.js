84afcaa71b8e66844e24b3c136ed3a12903058e2
KetanAggarwal26 <155048369+KetanAggarwal26@users.noreply.github.com>
2025-05-28 17:54:47 +0000

    conversion of datattypes

---
+++ 01basics/datatypeConversion.js
@@ -0,0 +1,34 @@
+let score='33'
+
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+
+console.log(typeof(valueInNumber));
+
+
+//"12" easily convert
+//'12abx' NaN
+//true 1 false=>0
+
+let isLoggedIn='33'
+
+console.log(typeof(isLoggedIn));
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+
+//"12" true
+//'' false
+// 1=>true 0=>false
+
+let someNumber=33
+
+console.log(typeof(someNumber));
+
+let stringsomeNumber=String(someNumber)
+
+console.log(typeof(stringsomeNumber));
+
+//33 string
+//'' null
+// true string
