df299d00838f4d5a7b428115c07c86b822814cbf
KetanAggarwal26 <155048369+KetanAggarwal26@users.noreply.github.com>
2025-05-29 18:00:23 +0000

    maths and num

--- 01basics/5_MathsandNumber.js
+++ 01basics/5_MathsandNumber.js
@@ -16,6 +16,23 @@ const hundredss= 100000
 
 //********maths*********
 
+console.log(Math.abs(-4)) // -ve to +ve
+console.log(Math.round(4.8)) // round off
+console.log(Math.ceil(4.3)) // round off top value
+console.log(Math.floor(4.8)) // round off below value
+console.log(Math.min(4.8,5,8,18,10)) // min value
+console.log(Math.random()) 
+console.log((Math.random()*10) +1) 
+
+const min=1
+const max=6
+console.log(Math.floor(Math.random() * (max - min +1))+min)
+
+
+
+
+
+
 
 
 
