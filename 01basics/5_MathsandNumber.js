98ee607bee050d3d91adcd9a0ac9c04086c38631
KetanAggarwal26 <155048369+KetanAggarwal26@users.noreply.github.com>
2025-05-29 17:41:07 +0000

    maths and Nums

---
+++ 01basics/5_MathsandNumber.js
@@ -0,0 +1,24 @@
+const score=400
+// console.log(score)
+// console.log(typeof(score))
+
+const balance= new Number(200.2121)
+// console.log(balance)
+// console.log(typeof(balance))
+// console.log(balance.toFixed(2))
+
+const precNum= new Number(200.8911)
+// console.log(precNum.toPrecision(3))
+// console.log(precNum.toPrecision(4))
+
+const hundredss= 100000
+// console.log(hundredss.toLocaleString('en-IN'))
+
+//********maths*********
+
+
+
+
+
+
+
