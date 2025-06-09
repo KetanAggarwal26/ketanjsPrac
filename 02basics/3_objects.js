31fe5f2019ccb4d50577efe573f7a46b433d65df
KetanAggarwal26 <155048369+KetanAggarwal26@users.noreply.github.com>
2025-06-09 20:51:23 +0000

    csdcscv

---
+++ 02basics/3_objects.js
@@ -0,0 +1,15 @@
+//singleton - Apni trha ka ek object constructor wala
+//Object.create()
+
+// object literals
+const userOne={
+    name:'Ketan',
+    age: 18,
+    location: Chandigarh,
+    isLoggedIn : false,
+    lastLoggedInDays:[
+        'Mon','Tue'
+    ]
+}
+console.log(userOne['name']) 
+console.log(userOne.name) 
